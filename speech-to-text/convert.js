eabe952052161749eae53acfa9f2bf5ef7742dad
Eadred Birchenough <eadred.birchenough@zuhlke.com>
2018-06-05 11:52:18 +0200

    Moved GCS manipulation into a separate module

--- speech-to-text/convert.js
+++ speech-to-text/convert.js
@@ -2,13 +2,11 @@ const linear16 = require('linear16');
 const fs = require('fs');
 const path = require('path');
 const replaceExt = require('replace-ext');
-const Storage = require('@google-cloud/storage');
 const rimraf = require('rimraf');
 const q = require('q');
 const _ = require('lodash');
+const bucketUtils = require('./bucketUtils');
 
-const projectId = 'eadred-project';
-const bucketName = 'zuhlke-mltt-camp2018'
 const inputLocalDir = path.join(__dirname, 'audio-in');
 const outputLocalDir = path.join(__dirname, 'audio-converted');
 const inputRemoteDir = 'audio/original';
@@ -26,51 +24,11 @@ if (fs.existsSync(outputLocalDir)) {
 }
 fs.mkdir(outputLocalDir);
 
-const storage = new Storage({
-  projectId: projectId
-});
-
-downloadAllInputFiles()
-.then(deleteAllOutputFiles)
+bucketUtils.downloadAllFiles(inputRemoteDir, inputLocalDir)
+.then(bucketUtils.deleteAllFiles(outputRemoteDir))
 .then(convertFiles)
-.then(() => console.log('All converted'))
-.then(uploadFiles)
-.then(() => console.log('All uploaded'))
+.then(() => bucketUtils.uploadAllFiles(outputLocalDir, outputRemoteDir, '.wav'))
 .catch(err => console.log(err));
-
-function listFiles(dirName) {
-  return storage.bucket(bucketName)
-  .getFiles({ directory: dirName})
-  .then(files => files[0].map(f => f.name))
-}
-
-function deleteAllOutputFiles() {
-  const deleteFile = remoteFileName => {
-    return storage.bucket(bucketName)
-    .file(remoteFileName)
-    .delete();
-  }
-
-  return listFiles(outputRemoteDir)
-  .then(fileNames => Promise.all(fileNames.map(deleteFile)))
-}
-
-function downloadAllInputFiles() {
-  return listFiles(inputRemoteDir)
-  .then(fileNames => Promise.all(fileNames.map(downloadFile)))
-  .then(() => console.log('All downloaded'))
-}
-
-function downloadFile(remoteFileName) {
-  const remoteFileNameParts = remoteFileName.split('/');
-  const localFileName = path.join(inputLocalDir, remoteFileNameParts[remoteFileNameParts.length - 1]);
-
-  console.log('Downloading ' + remoteFileName);
-
-  return storage.bucket(bucketName)
-  .file(remoteFileName)
-  .download({ destination: localFileName });
-}
 
 function convertFiles() {
   
@@ -96,28 +54,5 @@ function convertFiles() {
     deferred.resolve(Promise.all(allConversions));
   });
 
-  return deferred.promise;
+  return deferred.promise.then(() => console.log('All converted'));
 }
-
-function uploadFiles() {
-  const deferred = q.defer();
-
-  fs.readdir(outputLocalDir, (err, files) => {
-    var allUploads = _.filter(files, f => path.extname(f) === '.wav')
-    .map(file => {
-      console.log('Uploading:', file);
-
-      return storage.bucket(bucketName)
-      .upload(path.join(outputLocalDir, file), { destination: path.join(outputRemoteDir, file)})
-      .catch(err => console.log('Failed to upload:', file, err));
-    });
-
-    deferred.resolve(Promise.all(allUploads));
-  });
-
-  return deferred.promise;
-}
-
-
-
-
